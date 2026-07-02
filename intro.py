a37b56b432280328f2979e498a6d51d2be993437
jaskiratsaini518 <jaskiratsaini518@gmail.com>
2026-07-01 19:30:11 -0700

    Intro to Python - lesson 1

---
+++ intro.py
@@ -0,0 +1,12 @@
+# print hello world
+print("Hello, World!")
+
+# print my name
+print("My name is Jaskirat!")
+
+# print a fun fact about myself
+# print I love playing basketball
+print("I love playing basketball.")
+
+# performing and printing a simple calculation
+print("The result of 3+5 is:", 3 + 5)
